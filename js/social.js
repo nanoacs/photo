9680d222394269d78c44b9d63f93e0555e3838d3
nanoacs <nanoapscosas@gmail.com>
2016-12-16 14:16:53 -0600

    Ejercicio - Galeria de imagenes
    Diseño responsivo

---
+++ js/social.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('.icon-menu').click(function(){
+		$('.redes').slideToggle('slow');
+	});
+
+	/*$('.imgs img').mouseover(function(){
+		$(this).fadeTo('slow',1);
+	});
+
+	$('.imgs img').mouseout(function(){
+		$(this).fadeTo('slow',.6);
+	});*/
+	
+});
